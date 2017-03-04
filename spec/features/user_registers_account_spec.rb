a776691bad4987440b2ef6460485dc07a28bd406
disc0ninja <disc0ninja77@gmail.com>
2017-03-04 16:28:43 -0600

    Feature: Authentication: User Registers Added links to navbar and nav drawer for new user registrations Updated devise locales to display messages with terminoligy that matches test specs (eg register and not sign up) Updated test spec for user registering to run all scenarios twice, once using nav bar links and once using nav drawer links

--- spec/features/user_registers_account_spec.rb
+++ spec/features/user_registers_account_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.feature "A user registers for a new account" do
 
-  scenario "A user successfully signs registers for a new account" do
+  scenario "A user successfully signs registers for a new account with nav bar links" do
     visit "/"
 
     click_link("nav-bar-register")
@@ -13,10 +13,10 @@ RSpec.feature "A user registers for a new account" do
 
     click_button "Register"
 
-    expect(page).to have_content "You have registered successfully"
+    expect(page).to have_content("Welcome! you have registered successfully.")
   end
 
-  scenario "A user fails to register with: an empty email" do
+  scenario "A user fails to register with: an empty email with nav bar links" do
     visit "/"
 
     click_link("nav-bar-register")
@@ -30,7 +30,7 @@ RSpec.feature "A user registers for a new account" do
     expect(page).to have_content "Email can't be blank"
   end
 
-  scenario "A user fails to register with: an empty password" do
+  scenario "A user fails to register with: an empty password with nav bar links" do
     visit "/"
 
     click_link("nav-bar-register")
@@ -44,7 +44,7 @@ RSpec.feature "A user registers for a new account" do
     expect(page).to have_content "Password can't be blank"
   end
 
-  scenario "A user fails to register with: an empty email and password" do
+  scenario "A user fails to register with: an empty email and password with nav bar links" do
     visit "/"
 
     click_link("nav-bar-register")
@@ -63,7 +63,7 @@ RSpec.feature "A user registers for a new account" do
     @john = User.create(email: "john@example.com", password: "password")
   end
 
-  scenario "A user fails to register with: mismatched passwords" do
+  scenario "A user fails to register with: mismatched passwords with nav bar links" do
     visit "/"
 
     click_link("nav-bar-register")
@@ -74,7 +74,82 @@ RSpec.feature "A user registers for a new account" do
 
     click_button "Register"
 
-    expect(page).to have_content "Email already in use"
+    expect(page).to have_content "Email has already been taken"
+  end
+
+  scenario "A user successfully signs registers for a new account with nav drawer links" do
+    visit "/"
+
+    click_link("nav-drawer-register")
+
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+
+    click_button "Register"
+
+    expect(page).to have_content("Welcome! you have registered successfully.")
+  end
+
+  scenario "A user fails to register with: an empty email with nav drawer links" do
+    visit "/"
+
+    click_link("nav-drawer-register")
+
+    fill_in "Email", with: ""
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+
+    click_button "Register"
+
+    expect(page).to have_content "Email can't be blank"
+  end
+
+  scenario "A user fails to register with: an empty password with nav drawer links" do
+    visit "/"
+
+    click_link("nav-drawer-register")
+
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: ""
+    fill_in "Password confirmation", with: ""
+
+    click_button "Register"
+
+    expect(page).to have_content "Password can't be blank"
+  end
+
+  scenario "A user fails to register with: an empty email and password with nav drawer links" do
+    visit "/"
+
+    click_link("nav-drawer-register")
+
+    fill_in "Email", with: ""
+    fill_in "Password", with: ""
+    fill_in "Password confirmation", with: ""
+
+    click_button "Register"
+
+    expect(page).to have_content "Password can't be blank"
+    expect(page).to have_content "Email can't be blank"
+  end
+
+  before do
+    @john = User.create(email: "john@example.com", password: "password")
+  end
+
+  scenario "A user fails to register with: mismatched passwords with nav drawer links" do
+    visit "/"
+
+    click_link("nav-drawer-register")
+
+    fill_in "Email", with: @john.email
+    fill_in "Password", with: @john.password
+    fill_in "Password confirmation", with: @john.password
+
+    click_button "Register"
+
+    expect(page).to have_content "Email has already been taken"
   end
 
 end
