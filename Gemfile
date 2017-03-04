f20c627b1dad1db850b4805e92dbcb6c3fefe190
disc0ninja <disc0ninja77@gmail.com>
2017-03-04 14:51:56 -0600

    Resolved all merge conflicts

--- Gemfile
+++ Gemfile
@@ -20,6 +20,8 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 # Google MDL Sass in a gem
 gem 'material_design_lite-sass'
+# dependency of material_design_lite-sass
+gem 'autoprefixer-rails'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,17 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require material
 //= require jquery
 //= require jquery_ujs
-<<<<<<< HEAD
-//= require turbolinks
-//= require_tree .
-=======
-//= require material
 //= require turbolinks
 //= require_tree .
 
 document.addEventListener('turbolinks:load', function() {
   componentHandler.upgradeDom();
 });
->>>>>>> styling
