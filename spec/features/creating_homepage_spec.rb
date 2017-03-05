1bd1b5617bd45d3d5d8987171ea473b89de10092
disc0ninja <disc0ninja77@gmail.com>
2017-03-04 18:02:50 -0600

    Testing: Feature: Home/Dashboard: Updated creating home spec to have a scneario for logged in user, and ensure logout links are available to logged in user but no to non logged in users. Also added check to make sure registration links are not available to logged in user, but are for non logged in users

--- spec/features/creating_homepage_spec.rb
+++ spec/features/creating_homepage_spec.rb
@@ -2,17 +2,49 @@ require "rails_helper"
 
 RSpec.feature "Home Page" do
 
-  scenario "A user visits the home page" do
+  before do
+    @john = User.create(email: "john@example.com", password: "password")
+  end
+
+  scenario "A non logged in user visits the home page" do
     visit "/"
 
     expect(page).to have_link "nav-bar-home"
     expect(page).to have_link "nav-drawer-home"
+
+    expect(page).not_to have_link "nav-bar-logout"
+    expect(page).not_to have_link "nav-drawer-logout"
 
     expect(page).to have_link "nav-bar-register"
     expect(page).to have_link "nav-drawer-register"
 
     expect(page).to have_link "nav-bar-login"
     expect(page).to have_link "nav-drawer-login"
+
+    expect(page).to have_link "nav-bar-athletes-den"
+    expect(page).to have_link "nav-drawer-athletes-den"
+
+    expect(page).to have_content("Workout Lounge")
+
+    expect(page).to have_content("Share your workout with friends and family")
+  end
+
+  scenario "A logged in user visits the home page" do
+    login_as(@john)
+    
+    visit "/"
+
+    expect(page).to have_link "nav-bar-home"
+    expect(page).to have_link "nav-drawer-home"
+
+    expect(page).to have_link "nav-bar-logout"
+    expect(page).to have_link "nav-drawer-logout"
+
+    expect(page).not_to have_link "nav-bar-register"
+    expect(page).not_to have_link "nav-drawer-register"
+
+    expect(page).not_to have_link "nav-bar-login"
+    expect(page).not_to have_link "nav-drawer-login"
 
     expect(page).to have_link "nav-bar-athletes-den"
     expect(page).to have_link "nav-drawer-athletes-den"
