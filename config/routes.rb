9bee68b08f711ec5d29e58c8ae909d3559fb75ca
disc0ninja <disc0ninja77@gmail.com>
2017-03-07 01:55:17 -0600

    Feature: Search for users Implemented search functionality for users Added method to search by name to user model Added route for dashboards search Updated dashboards controller to have search action using pagination updated navigation to display search bar and buttons

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'dashboards#index'
 
+  resources :dashboards, only: [:index] do
+    collection do
+      post :search, to: 'dashboards#search'
+    end
+  end
+
   resources :users do
     resources :exercises
   end
--- spec/features/dashboard/search_for_users_spec.rb
+++ spec/features/dashboard/search_for_users_spec.rb
@@ -6,6 +6,8 @@ RSpec.feature "Serach for user:" do
     @john = User.create(first_name: "John", last_name: "Doe", email: "john@example.com", password: "password")
 
     @sarah = User.create(first_name: "Sarah", last_name: "Doe", email: "sarah@example.com", password: "password")
+
+    @joe = User.create(first_name: "Joe", last_name: "Dirt", email: "joe@dirt.com", password: "password")
   end
 
   scenario "with existing name, returns all users with searched name" do
@@ -13,10 +15,11 @@ RSpec.feature "Serach for user:" do
 
     fill_in "search-name", with: "Doe"
 
-    click_button "Search"
+    click_button("search")
 
     expect(page).to have_content(@john.full_name)
     expect(page).to have_content(@sarah.full_name)
+    expect(page).not_to have_content(@joe.full_name)
     expect(current_path).to eq("/dashboards/search")
   end
 
