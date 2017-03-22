ca8f9c76be7f85067f8154f64ce5c0d87660ec4b
disc0ninja <disc0ninja77@gmail.com>
2017-03-21 21:28:22 -0500

    Feature: Chat: Implemented chat feature on 'my lounge' page(s) Updated exercises controller to set room, messages and followers on index action Added messages partial (chat messages) that displays all messages in the exercises index view
    added resources for chat messages to be created
    updated associations for users and rooms to have many messages
    
    removed pending specs

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,6 @@
 class Room < ApplicationRecord
   belongs_to :user
+
+  has_many :messages
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
   resources :users do
     resources :exercises
   end
+
   resources :friendships, only: [:show, :create, :destroy]
+
+  resources :messages, only: [:create]
 
 end
